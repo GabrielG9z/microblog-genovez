d0883fa05e2ef59c18c4bf13d843dca07c02ce5d
Gabriel Genovez <86001324+GabrielG9z@users.noreply.github.com>
2022-07-27 08:46:29 -0300

    Update
    Fizemos o código de exclusão de usuários do microblog

---
+++ admin/usuario-exclui.php
@@ -0,0 +1,17 @@
+<?php
+
+use Microblog\Usuario;
+
+require_once '../vendor/autoload.php';
+
+//Criamos um objeto para poder acessar os recursos da classe,  
+$usuario = new Usuario;
+
+//obtemos o ID da URL primeiramente e passamos para o setter,
+$usuario->setId($_GET['id']);
+
+//só então executamos a exclusão
+$usuario->excluir();
+
+//Após excluir, redirecionamos para a página de lista de usuários.
+header("location:usuarios.php");
